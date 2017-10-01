7a5890afc4ecd4a98c148c2b687cc5b2a6fceb66
QigangZhong <qigangzhong@hotmail.com>
2017-10-01 13:32:05 +0800

    user management

--- src/main/java/com/pavilion/Application.java
+++ src/main/java/com/pavilion/Application.java
@@ -1,7 +1,8 @@
 package com.pavilion;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -10,11 +11,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @MapperScan("com.pavilion.dao")
 @ServletComponentScan
 public class Application {
-    static {
-
-    }
-
+    static Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
+
+        logger.info("启动应用");
         SpringApplication.run(Application.class,args);
     }
 }
